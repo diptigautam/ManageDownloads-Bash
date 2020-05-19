24995f008cce56eac5cbd771003f054b81fdc93e
Dipti Gautam <diptigautam01@gmail.com>
2020-05-19 22:46:53 +0545

    Add separators to differentiate processes

--- manage_downloads.sh
+++ manage_downloads.sh
@@ -1,23 +1,40 @@
 #!/usr/bin/env bash
 # File: manage_downloads.sh
 
+function separate_internal {
+	echo "***************************************"
+}
+
+function separate_external {
+	echo "#######################################"
+}
+
 function general {
 	if [ -f *$2 ]
 	then
 		echo "Process staring to move the $2 files"
+		separate_internal
         	if [ -d "$1" ]
         	then
                		echo "Directory already exists"
+			separate_internal
         	else
                 	echo "Creating directory $1"
+			separate_internal
                 	$(mkdir $1)
+			separate_internal
                 	echo "Directory created."
+			separate_internal
         	fi
                 echo "Moving $2 files to /$1"
+		separate_internal
                 $(mv *$2 $1/)
+		separate_internal
                 echo "Process to move $2 files completed."
+		separate_external
 	else
 		echo "No files of extension $2"
+		separate_external
 	fi
 }
 
@@ -25,9 +42,14 @@ function general {
 
 function organize {
 	files=$(ls -1Ap | grep -v /)	
-       	echo $files
+       	echo "Listing all files in the directory..."
+	separate_internal
+	echo $files
+	separate_internal
 	general "text_files" ".txt"
+	separate_internal
 	general "PDFs" ".pdf"
+	separate_external
 }
 
 organize
