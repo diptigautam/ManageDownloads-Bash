5bdc5e8b0364ec37b892cbeee76669e9a677fc6f
Dipti Gautam <diptigautam01@gmail.com>
2020-05-19 22:38:16 +0545

    Make generic function better to check file existence before conducting operations

--- manage_downloads.sh
+++ manage_downloads.sh
@@ -2,18 +2,23 @@
 # File: manage_downloads.sh
 
 function general {
-        echo "Process staring to move the $2 files"
-        if [ -d "$1" ]
-        then
-                echo "Directory already exists"
-        else
-                echo "Creating directory $1"
-                $(mkdir $1)
-                echo "Directory created."
-        fi
+	if [ -f *$2 ]
+	then
+		echo "Process staring to move the $2 files"
+        	if [ -d "$1" ]
+        	then
+               		echo "Directory already exists"
+        	else
+                	echo "Creating directory $1"
+                	$(mkdir $1)
+                	echo "Directory created."
+        	fi
                 echo "Moving $2 files to /$1"
                 $(mv *$2 $1/)
                 echo "Process to move $2 files completed."
+	else
+		echo "No files of extension $2"
+	fi
 }
 
 
