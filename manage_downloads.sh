9eafeb814c12ddd97ea1bfea7ec6408c87312c28
Dipti Gautam <diptigautam01@gmail.com>
2020-05-19 10:17:39 +0545

    Abstract text_files into a function

--- manage_downloads.sh
+++ manage_downloads.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 # File: manage_downloads.sh
 
+function text_files {
+	echo "Process staring to move the .txt files"
+        if [ -d "text_files" ]
+        then
+                echo "Directory already exists"
+        else
+                echo "Creating directory text_files"
+                $(mkdir text_files)
+                echo "Directory created."
+	fi
+                echo "Moving .txt files to /text_files"
+                $(mv *.txt text_files/)
+                echo "Process to move .txt files completed."
+}
+
 function organize {
 	files=$(ls -1Ap | grep -v /)	
        	echo $files
-	echo "Process staring to move the .txt files"
-	if [ -d "text_files" ]
-	then
-		echo "Directory already exists"
-		echo "Moving .txt files to /text_files"
-		$(mv *.txt text_files/)
-		echo "Process to move .txt files completed."
-	else
-		echo "Creating directory text_files"
-		$(mkdir text_files)
-		echo "Directory created"
-		echo "Moving .txt files to /text_files"
-		$(mv *.txt text_files/)
-		echo "Process to move .txt files completed."
-	fi
+	text_files
 }
 
 organize
