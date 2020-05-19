630b842117261b1e1f2271289dd8ee1ec84db318
Dipti Gautam <diptigautam01@gmail.com>
2020-05-19 09:48:36 +0545

    Manage text files

---
+++ manage_downloads.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# File: manage_downloads.sh
+
+function organize {
+	files=$(ls -1Ap | grep -v /)	
+       	echo $files
+	echo "Process staring to move the .txt files"
+	if [ -d "/text_files" ]
+	then
+		echo "Directory already exists"
+		echo "Moving .txt files to /text_files"
+		$(mv *.txt /text_files)
+		echo "Process to move .txt files completed."
+	else
+		echo "Creating directory text_files"
+		$(mkdir text_files)
+		echo "Directory created"
+		echo "Moving .txt files to /text_files"
+		$(mv *.txt text_files/)
+		echo "Process to move .txt files completed."
+	fi
+}
+
+organize
