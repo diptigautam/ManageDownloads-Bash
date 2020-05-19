742d837c819d1513b8dbfe4f3fd2475e885dc466
Dipti Gautam <diptigautam01@gmail.com>
2020-05-19 22:29:18 +0545

    Remove the specific function for .txt files

--- manage_downloads.sh
+++ manage_downloads.sh
@@ -1,21 +1,6 @@
 #!/usr/bin/env bash
 # File: manage_downloads.sh
 
-function text_files {
-	echo "Process staring to move the .txt files"
-        if [ -d "text_files" ]
-        then
-                echo "Directory already exists"
-        else
-                echo "Creating directory text_files"
-                $(mkdir text_files)
-                echo "Directory created."
-	fi
-                echo "Moving .txt files to /text_files"
-                $(mv *.txt text_files/)
-                echo "Process to move .txt files completed."
-}
-
 function general {
         echo "Process staring to move the $2 files"
         if [ -d "$1" ]
