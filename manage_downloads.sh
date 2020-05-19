5a9dcbfd53b17a4838012afd3dc3b359cdaedd32
Dipti Gautam <diptigautam01@gmail.com>
2020-05-19 22:28:12 +0545

    Generic function to perform the move and check operation

--- manage_downloads.sh
+++ manage_downloads.sh
@@ -16,10 +16,28 @@ function text_files {
                 echo "Process to move .txt files completed."
 }
 
+function general {
+        echo "Process staring to move the $2 files"
+        if [ -d "$1" ]
+        then
+                echo "Directory already exists"
+        else
+                echo "Creating directory $1"
+                $(mkdir $1)
+                echo "Directory created."
+        fi
+                echo "Moving $2 files to /$1"
+                $(mv *$2 $1/)
+                echo "Process to move $2 files completed."
+}
+
+
+
 function organize {
 	files=$(ls -1Ap | grep -v /)	
        	echo $files
-	text_files
+	general "text_files" ".txt"
+	general "PDFs" ".pdf"
 }
 
 organize
